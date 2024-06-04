4f7e5b12da3db5343cfffc4ef239973788a69383
Matt <mateo19dg@gmail.com>
2024-06-04 18:08:21 -0500

    Iniciando el proyecto

---
+++ app/src/main/java/com/example/appandroiddemo1/components.kt
@@ -0,0 +1,11 @@
+package com.example.appandroiddemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class components : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_components)
+    }
+}
